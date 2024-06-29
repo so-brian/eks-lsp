ead52f04105da2cbf2c44cfa8d184c90c41b8f44
Brian Ding <brian_ding@outlook.com>
2024-06-29 21:35:40 +0800

    init project

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import {
+	createConnection,
+	TextDocuments,
+	ProposedFeatures} from 'vscode-languageserver/node';
+
+import {
+	TextDocument
+} from 'vscode-languageserver-textdocument';
+
+// Create a connection for the server, using Node's IPC as a transport.
+// Also include all preview / proposed LSP features.
+const connection = createConnection(ProposedFeatures.all);
+
+// Create a simple text document manager.
+const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+
+// Listen on the connection
+connection.listen();
+
