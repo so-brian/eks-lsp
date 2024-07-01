b59b7bf703a177b43f936146bfc17f52c78c585e
Brian Ding <brian_ding@outlook.com>
2024-07-01 22:03:57 +0800

    handle initialize

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,7 @@
-import {
-	createConnection,
-	TextDocuments,
-	ProposedFeatures} from 'vscode-languageserver/node';
+import { createConnection, TextDocuments, ProposedFeatures } from 'vscode-languageserver/node';
 
-import {
-	TextDocument
-} from 'vscode-languageserver-textdocument';
+import { TextDocument } from 'vscode-languageserver-textdocument';
+import { InitializeHandler } from './handlers';
 
 // Create a connection for the server, using Node's IPC as a transport.
 // Also include all preview / proposed LSP features.
@@ -14,10 +10,11 @@ const connection = createConnection(ProposedFeatures.all);
 // Create a simple text document manager.
 const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
 
+connection.onInitialize(InitializeHandler);
+
 // Make the text document manager listen on the connection
 // for open, change and close text document events
 documents.listen(connection);
 
 // Listen on the connection
 connection.listen();
-
